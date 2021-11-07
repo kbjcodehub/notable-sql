6ad1988c008c5e3b8bd344df9b879a1a97192fcb
kbj <kyei67@gmail.com>
2021-11-07 23:58:16 +0000

    connect to cloud mongodb successfully

--- server.js
+++ server.js
@@ -2,9 +2,27 @@ const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
 const app = express();
-
+const db = require('./app/config/db');
 const port_number = 8081;
 
-app.listen(port_number, ()=>{
-    console.log('we are live on ' + port_number)
+app.use(bodyParser.urlencoded({ extended: true }));
+
+MongoClient.connect(db.url, (err, database) => {
+    if (err) return console.log(err);
+
+    var adminDb = database.db("Demo").admin();
+    // List all the available databases
+    adminDb.listDatabases(function (err, result) {
+
+        if (err) return console.log(err);
+        console.log(result.databases);
+        database.close();
+    });
+
+    require('./app/routes/')(app, database);
+    app.listen(port_number, () => {
+        console.log('service listening on... ' + port_number)
+    });
+
 });
+
