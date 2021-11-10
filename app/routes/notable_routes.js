f70421371b5f7ee979d59465ae702f4261a39907
kbj <kyei67@gmail.com>
2021-11-10 19:03:05 +0000

    add insert collection method

---
+++ app/routes/notable_routes.js
@@ -0,0 +1,19 @@
+const { request } = require("express");
+
+module.exports = function (app, db) {
+    app.post('/notes', (req, res) => {
+        console.log('post note method ');
+        console.log(req.body);
+
+        const newNote = {text: req.body.body, title: req.body.title}
+
+        db.collection('Notes').insert(newNote, (err, result) => {
+            if(err){
+                return res.send("An error ocurred, please try agian...")
+            }
+
+            console.log(result)
+            res.send('Successful');
+        });
+    });
+};
