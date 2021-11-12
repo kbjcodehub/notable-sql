c647ad7c0b4fb389cc235e4a8f89a74ac7c3edba
kbj <kyei67@gmail.com>
2021-11-12 08:38:57 +0000

    add delete and update route

--- app/routes/notable_routes.js
+++ app/routes/notable_routes.js
@@ -2,37 +2,77 @@ const { request } = require("express");
 var ObjectID = require('mongodb').ObjectID;
 
 module.exports = function (app, db) {
+    app.post('/notes', (req, res) => {
+        console.log('post note method ');
+        console.log(req.body);
+
+        const newNote = { text: req.body.body, title: req.body.title }
+
+        db.collection('Notes').insertOne(newNote, (err, result) => {
+            if (err) {
+                return res.send("An error ocurred, please try agian...")
+            }
+
+            console.log(result.acknowledged);
+            res.send('Successful');
+        });
+    });
+
     app.get('/notes/:id', (req, res) => {
         console.log('get note method ');
+
         const id = req.params.id;
         console.log(id);
 
-        const details = { '_id': new ObjectID(id)
-    };
+        const details = {
+            '_id': new ObjectID(id)
+        };
 
-    console.log(details);
-    db.collection('Notes').findOne(details, (err, item) => {
+        console.log(details);
+        db.collection('Notes').findOne(details, (err, item) => {
 
-        if (err) {
-            return res.send("An error ocurred, please try agian...")
-        }
-        console.log(item);
-        res.send(item);
+            if (err) {
+                return res.send("An error ocurred, please try agian...")
+            }
+            console.log(item);
+            res.send(item);
+        });
     });
-});
-app.post('/notes', (req, res) => {
-    console.log('post note method ');
-    console.log(req.body);
 
-    const newNote = { text: req.body.body, title: req.body.title }
+    app.delete('/notes/:id', (req, res) => {
+        console.log('delete note method ');
 
-    db.collection('Notes').insertOne(newNote, (err, result) => {
-        if (err) {
-            return res.send("An error ocurred, please try agian...")
-        }
+        const id = req.params.id;
+        console.log(id);
 
-        console.log(result.acknowledged);
-        res.send('Successful');
+        const details = { '_id': new ObjectID(id) };
+
+        db.collection('Notes').remove(details, (err, item) => {
+
+            if (err) {
+                return res.send("An error ocurred, please try agian...")
+            }
+            console.log(item);
+            res.send(item);
+        });
+
     });
-});
+
+    app.put('/notes/:id', (req, res) => {
+        console.log('update note method ');
+
+        const id = req.params.id;
+        console.log(id);
+
+        const details = { '_id': new ObjectID(id) };
+        const note = { text: req.body.body, title: req.body.title };
+
+        db.collection('Notes').replaceOne(details, note, (err, result) => {
+            if (err) {
+                return res.send("An error ocurred, please try agian...")
+            }
+            console.log(result);
+            res.send(result);
+        });
+    });
 };
